2f29dfedda53f75cf353a9add6311adf91a44af8
artemmedvedev31og <artemw2017@gmai.com>
2026-07-14 12:24:40 +0400

    Initial commit: Spring Boot project structure and DB config

---
+++ src/main/java/com/example/abriliryc/AbrilirycApplication.java
@@ -0,0 +1,13 @@
+package com.example.abriliryc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrilirycApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(AbrilirycApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abriliryc/AbrilirycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abriliryc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrilirycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
